9619c26df2b6b2c41d8347d8cf2aae1a0e6555ea
Igor Brito <37213793+igorcbrito@users.noreply.github.com>
2019-04-10 14:37:06 -0300

    Corrigido alguns erros
    Falta subtração, portar o multiplexador para duas entradas e pinar

--- multiplexador/inversor.vhd
+++ multiplexador/inversor.vhd
@@ -1,16 +1,32 @@
+library ieee;
 use ieee.std_logic_1164.all;
 
 entity inversor is
 port(
   A      : in std_logic_vector(3 downto 0);
-  y		: out std_logic
+  y		: out std_logic_vector(3 downto 0)
   );
 end inversor;
  
 architecture archInversor of inversor is
 begin
 
-    y <= (A(3) and not(B(3))) or ((A(3) xor B(3)) and (A(2) and not B(2))) or ((A(3) xor B(3)) and (A(2) xor B(2)) and (A(1) and not(B(1)))) or
-	 ((A(3) xor B(3)) and (A(2) xor B(2)) and (A(1) xor B(1)) and (A(0) and not(B(0))));
-					
+    WITH A SELECT
+    y <= not("0000") WHEN "0000",
+                 not("0001") WHEN "0001",
+                 not("0010") WHEN "0010",
+                 not("0011") WHEN "0011",
+                 not("0100") WHEN "0100",
+                 not("0101") WHEN "0101",
+                 not("0110") WHEN "0110",
+                 not("0111") WHEN "0111",
+                 not("1000") WHEN "1000",
+                 not("1001") WHEN "1001",
+					  not("1010") WHEN "1010",
+					  not("1011") WHEN "1011",
+					  not("1100") WHEN "1100",
+					  not("1101") WHEN "1101",
+					  not("1110") WHEN "1110",
+					  not("1111") WHEN "1111",
+					  not("0000") WHEN OTHERS;
 end archInversor;
--- multiplexador/multiplexador.vhd
+++ multiplexador/multiplexador.vhd
@@ -2,42 +2,42 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity multiplexador is
-	port (
-		x0,x1,x2,x3: in std_logic;
-		s2,s1,s0: in std_logic;
-		y: out std_logic
+	port(
+			x0,x1,x2,x3: in std_logic;
+			s2,s1,s0: in std_logic;
+			y: out std_logic
 		);
 end multiplexador;
 
+architecture seletor of multiplexador is
+
 component soma is
 	port(
-		x0, x1: in std_logic;
-		resultado_soma: out std_logic;	
+			x0, x1: in std_logic;
+			resultado_soma: out std_logic	
 		);
 end component;
 
 component inversor is
 	port(
-		A: in std_logic;
-		resultado_inversor: out std_logic;
+			A: in std_logic;
+			resultado_inversor: out std_logic
 		);
 end component;
 
 component maiorque is
 	port(
-		x0, x1: in std_logic;
-		resultado_mairoque: out std_logic;
+			x0, x1: in std_logic;
+			resultado_mairoque: out std_logic
 		);
 end component;
 
 component menorque is
 	port(
-		x0,x1: in std_logic;
-		resultado_menorque: out std_logic;
+			x0,x1: in std_logic;
+			resultado_menorque: out std_logic
 		);
 end component;
-
-architecture seletor of multiplexador is
 begin
 
 		y <= (x0 and (not(s1)and not(s0))) or (x1 and (not(s1) and s0)) or (x2 and (s1 and not(s0))) or (x3 and (s1 and s0));
