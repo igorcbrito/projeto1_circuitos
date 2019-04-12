c89be48bed3fcf3b5d669fee9605c97d1ba92eb5
Igor Brito <37213793+igorcbrito@users.noreply.github.com>
2019-04-12 18:17:32 -0300

    Multiplexador funcionando
    Falta só incluir a subtração que está no meio email, pinar e testar na placa. Precisa fazer o relatório ainda.

--- conversor.vhd
+++ conversor.vhd
@@ -4,27 +4,40 @@ use ieee.std_logic_1164.all;
 entity conversor is
 port(
   I      : in std_logic_vector(3 downto 0);
-  Y		: out std_logic_vector(6 downto 0));
+  Y		: out std_logic_vector(6 downto 0);
+  ctrl	: in std_logic_vector(1 downto 0)
+  );
 end conversor;
  
 architecture archConversor of conversor is
+
+signal Yconversor : std_logic_vector(6 downto 0);
+
 begin
+ 
   WITH I SELECT
-    Y <= not("1111110") WHEN "0000",
-                 not("0110000") WHEN "0001",
-                 not("1101101") WHEN "0010",
-                 not("1111001") WHEN "0011",
-                 not("0110011") WHEN "0100",
-                 not("1011011") WHEN "0101",
-                 not("1011111") WHEN "0110",
-                 not("1110000") WHEN "0111",
-                 not("1111111") WHEN "1000",
-                 not("1111011") WHEN "1001",
-					  not("1110111") WHEN "1010",
-					  not("0011111") WHEN "1011",
-					  not("1001110") WHEN "1100",
-					  not("0111101") WHEN "1101",
-					  not("1001111") WHEN "1110",
-					  not("1000111") WHEN "1111",
-					  not("0000000") WHEN OTHERS;
+    Yconversor <= not("1111110") WHEN "0000",
+                  not("0110000") WHEN "0001",
+                  not("1101101") WHEN "0010",
+                  not("1111001") WHEN "0011",
+                  not("0110011") WHEN "0100",
+                  not("1011011") WHEN "0101",
+                  not("1011111") WHEN "0110",
+                  not("1110000") WHEN "0111",
+                  not("1111111") WHEN "1000",
+                  not("1111011") WHEN "1001",
+					   not("1110111") WHEN "1010",
+					   not("0011111") WHEN "1011",
+					   not("1001110") WHEN "1100",
+					   not("0111101") WHEN "1101",
+					   not("1001111") WHEN "1110",
+					   not("1000111") WHEN "1111",
+					   not("0000000") WHEN OTHERS;
+			
+	WITH ctrl SELECT
+		Y <=  Yconversor 		 WHEN "00",
+				not("1101111")	 WHEN "01",
+				"0000000"	 	 WHEN "10",
+				"0000000"		 WHEN OTHERS;
+				
 end archConversor;
--- multiplexador.vhd
+++ multiplexador.vhd
@@ -4,9 +4,10 @@ use ieee.std_logic_1164.all;
 entity multiplexador is
 	port(
 			x0,x1		: in std_logic_vector(3 downto 0);
-			S			: in std_logic_vector(3 downto 0);
+			S			: in std_logic_vector(2 downto 0);
 			ctrl		: in std_logic_vector(2 downto 0);
-			y			: out std_logic_vector(3 downto 0)
+			y			: out std_logic_vector(6 downto 0);
+			desliga   : in std_logic
 		);
 end multiplexador;
 
@@ -46,28 +47,31 @@ end component;
 component conversor is
 		port(
 				I		: in std_logic_vector(3 downto 0);
-				Y		: out std_logic_vector(6 downto 0))
+				Y		: out std_logic_vector(6 downto 0);
+				ctrl	 : in std_logic_vector(1 downto 0)
 		);
 end component;
 
-signal ySoma, yMaior, yMenor, yInvertido, yResultado : std_logic_vector(3 downto 0);
+signal ySoma, yMaior, yMenor, yInvertido : std_logic_vector(3 downto 0);
+signal yResultado: std_logic_vector(3 downto 0);
+signal controle : std_logic_vector(1 downto 0);
 
 begin
-total : soma port map(x0, x1, ySoma);
-maior : maiorque port map(x0, x1, yMaior(0));
-menor : menorque port map(x0, x1, yMenor(0));
-invertido : inversor port map(x1, yInvertido);
+total 		: soma port map(x0, x1, ySoma);
+maior 		: maiorque port map(x0, x1, yMaior(0));
+menor 		: menorque port map(x0, x1, yMenor(0));
+invertido	: inversor port map(x1, yInvertido);
+converte 	: conversor port map(yResultado, y, controle);
 
 	WITH S SELECT
-		y <=  ySoma      WHEN "001",
-				yMaior     WHEN "011", 
-				yMenor     WHEN "100",
-				yInvertido WHEN "101",
-				"0000"     WHEN OTHERS;
+		yResultado <=  ySoma      WHEN "001",
+							yMaior     WHEN "011", 
+							yMenor     WHEN "100",
+							yInvertido WHEN "101",
+							"0000"     WHEN OTHERS;
+							
+	controle(0) <= '0' and not(desliga); -- substituir 0 por erro da subtração
+	controle(1) <= desliga;
 				
-	WITH ctrl SELECT
-		y <=  yResultado   WHEN "00"
-				Erro 			 WHEN "01"
-				desligado	 WHEN "10"
 		
 end seletor;
